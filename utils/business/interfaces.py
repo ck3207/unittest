dafff0a1c20afa8d225349e3c06f834ee8e1e770
ck3207 <31480811+ck3207@users.noreply.github.com>
2020-04-30 16:23:01 +0800

    增加了投顾项目本地注册用户的接口

--- utils/business/interfaces.py
+++ utils/business/interfaces.py
@@ -17,20 +17,39 @@ class Interfaces:
         pass
 
     def request(self, url, data, is_get_method=True, verify=False, headers={}):
-        print("Will Reuqeuest Interface: {}".format(url))
-        print("Augues as follows: \n{}".format(data))
+        # print("Will Reuqeuest Interface: {}".format(url))
+        # print("Augues as follows: \n{}".format(data))
         if not headers:
             headers = Interfaces.HEADERS
         if is_get_method:
             r = requests.get(url=url, headers=headers, verify=verify, params=data)
         else:
-            r = requests.post(url=url, headers=headers, verify=verify, params=data)
+            r = requests.post(url=url, headers=headers, verify=verify, params=data, data=data)
         result = r.json()
-        print("Response: \n{}".format(result))
+        # print("Response: \n{}".format(result))
         return result
 
     def get_url_prefix(self, url_name="ANALYSIS_FC_PREFIX"):
         return Interfaces.url_name
 
-
+import time
 interfaces = Interfaces()
+url = "http://10.20.37.227:8088/vipstu/v1/func_mobile_login_register_by_tenant"
+data = {
+    "tenant_key": "228a888fb5e442fbad79667bec844a82",
+    "customer_avatar": "123",
+    "password": "e10adc3949ba59abbe56e057f20f883e"
+}
+for mobile in range(19990000402, 19999999999):
+    data["mobile"] = str(mobile)
+    data["customer_name"] = str(mobile)[:3]+"*****"+str(mobile)[-4:]
+    result = interfaces.request(url=url, data=str(data), is_get_method=False, verify=False)
+    try:
+        if result.get("data")[0].get("error_no") != "0":
+            print("Request Param:\n{0}".format(data))
+            print("Response: \n{0}".format(result))
+    except Exception as e:
+        print(str(e))
+        print("Request Param:\n{0}".format(data))
+        print("Response: \n{0}".format(result))
+    time.sleep(10)
